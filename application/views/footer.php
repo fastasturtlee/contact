263dbbca3e2839db8c4ce6f9f3d0b4ef149ee1c7
shaan2727 <jslshashank27@gmail.com>
2021-07-13 11:52:51 +0530

    unset session data

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,8 @@
 
 </body>
 </html>
+<?php
+if($this->session->has_userdata('title','fname','lname','filename','dob','mobno','email','message')){
+    $this->session->unset_userdata('title','fname','lname','filename','dob','mobno','email','message');
+}
+?>
